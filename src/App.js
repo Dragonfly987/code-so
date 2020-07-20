4bde7e344a6b31d868f31ae8c3b15471b455fe56
munozjuo987 <66138587+Dragonfly987@users.noreply.github.com>
2020-07-20 12:00:15 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hola chicos</h1>
+      <p>Esto es React desde CodeSandbox</p>
+      <p className="text-color"> wenas</p>
+    </div>
+  );
+}
